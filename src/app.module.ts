598c848bfbb4ef22b186bc5321e5d784ec07d3e2
sio <cazador.min@gmail.com>
2024-08-28 15:55:00 +0900

    refactor: add orm-config.provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { databaseConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { CommentsModule } from 'src/comments/comments.module';
+import OrmConfigProvider from 'src/config/orm-config.provider';
 import { PostsModule } from 'src/posts/posts.module';
 import { UsersModule } from 'src/users/users.module';
 
+console.log(process.env.NODE_ENV);
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(databaseConfig),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => OrmConfigProvider.forRoot(),
+    }),
     PostsModule,
     CommentsModule,
     UsersModule,
